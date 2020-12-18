47fbddc8341d8f181e9cd0ede6ea62778c2f2816
VJarreta <738737@unizar.es>
2020-12-18 17:15:47 +0100

    Added max:x

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,8 +1,11 @@
 package soa.eip;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+// https://camel.apache.org/manual/latest/processor.html
 @Component
 public class Router extends RouteBuilder {
 
@@ -12,8 +15,23 @@ public class Router extends RouteBuilder {
   public void configure() {
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
-      .log("Searching twitter for \"${body}\"!")
-      .toD("twitter-search:${body}")
+      .log("Searching twitter for \"${body}\"!").process(exchange -> {
+        String payload = exchange.getIn().getBody(String.class);
+        // do something with the payload and/or exchange here
+        if (payload.matches("^[a-zA-Z0-9 ]+ max:[0-9]+$")) {
+          //It contains "max:x"
+          int count = Integer.parseInt(payload.split(":")[1]);
+          //Delete "max:x" from search first
+          exchange.getIn().setBody(payload.replace("max:" + count, ""));
+          // Get x and set it as header
+          exchange.getIn().setHeader("count", count);
+        } else {
+          //Default count value
+          exchange.getIn().setHeader("count", 5);
+        }
+      })
+      //Set header value as count in search
+      .toD("twitter-search:${body}?count=${header.count}")
       .log("Body now contains the response from twitter:\n${body}");
   }
 }
